bb17404b2b9200f48e0eca55688ed0d781cf65bd
Madalin Ignisca <git@madalin.me>
2022-07-01 10:47:53 +0200

    Restore web middleware | For PWA web applications

--- src/Http/Kernel.php
+++ src/Http/Kernel.php
@@ -2,13 +2,16 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\EncryptCookies;
 use App\Http\Middleware\TrimStrings;
 use App\Http\Middleware\TrustHosts;
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
 use Illuminate\Auth\Middleware\Authorize;
 use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 use Illuminate\Auth\Middleware\RequirePassword;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
 use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
@@ -19,7 +22,12 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Routing\Middleware\ValidateSignature;
 use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
+/**
+ * Http Kernel
+ */
 class Kernel extends HttpKernel
 {
     /**
@@ -44,6 +52,14 @@ class Kernel extends HttpKernel
      * @var array<string, array<int, class-string|string>>
      */
     protected $middlewareGroups = [
+        'web' => [
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            VerifyCsrfToken::class,
+            SubstituteBindings::class,
+        ],
         'api' => [
             'throttle:api',
             SubstituteBindings::class,
